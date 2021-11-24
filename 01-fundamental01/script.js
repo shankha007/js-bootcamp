8ee837749af334910ed1079de94750165e76f7b1
Shankha <ribhumaster@gmail.com>
2021-11-24 09:21:32 +0530

    initial commit

---
+++ 01-fundamental01/script.js
@@ -0,0 +1,17 @@
+// let js = "amazing";
+// console.log(10 + 645 + 254 + 236);
+
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Rega";
+// console.log(firstName);
+
+// let age = 31;
+// age = 32;
+
+// const birthYear = 1991;
+// birthYear = 1998; // Will throw error
+
+// lastName = "Das"; // Don't declare variables like this, it will not throw error but will be a part of global object
+// console.log(lastName);
